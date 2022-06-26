9f55441ff351ad6d791b252ef130a17d6760df7f
Simon <tengentoppa@hotmail.com.tw>
2022-06-26 22:34:25 +0800

    skip migration if os is not linux or macos

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,41 +12,43 @@ import { TasksModule } from './tasks/task-test.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
-// @Injectable()
-// export class AppModule implements OnApplicationBootstrap {
-//   async onApplicationBootstrap(): Promise<void> {
-//     // exec migration script
-//     // await this.runMigration();
-//     // filled up with initial data list
-//     console.log(`The module has been initialized.`);
-//   }
-//   async runMigration(): Promise<void> {
-//     const path = './startScript.sh';
-//     try {
-//       if (fs.existsSync(path)) {
-//         console.log('found startScript.sh, executing migration');
-//       } else {
-//         console.log('startScript.sh not found');
-//         process.exit(1);
-//       }
-//     } catch (err) {
-//       console.error(err);
-//       process.exit(1);
-//     }
-//     await this.executeScript(path);
-//   }
+@Injectable()
+export class AppModule implements OnApplicationBootstrap {
+  async onApplicationBootstrap(): Promise<void> {
+    // exec migration script
+    const linux_liked_os = ['linux', 'darwin'];
+    if (linux_liked_os.includes(process.platform)) {
+      await this.runMigration();
+    }
+    // filled up with initial data list
+    console.log(`The module has been initialized.`);
+  }
+  async runMigration(): Promise<void> {
+    const path = './startScript.sh';
+    try {
+      if (fs.existsSync(path)) {
+        console.log('found startScript.sh, executing migration');
+      } else {
+        console.log('startScript.sh not found');
+        process.exit(1);
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    await this.executeScript(path);
+  }
 
-//   async executeScript(path): Promise<string> {
-//     return new Promise((callback) => {
-//       exec('sh ' + path, (error, stdout, stderr) => {
-//         console.log(stdout);
-//         if (error !== null) {
-//           console.log(`exec error: ${error}`);
-//           return;
-//         }
-//         callback('Script finished success');
-//       });
-//     });
-//   }
-// }
+  async executeScript(path): Promise<string> {
+    return new Promise((callback) => {
+      exec('sh ' + path, (error, stdout, stderr) => {
+        console.log(stdout);
+        if (error !== null) {
+          console.log(`exec error: ${error}`);
+          return;
+        }
+        callback('Script finished success');
+      });
+    });
+  }
+}
