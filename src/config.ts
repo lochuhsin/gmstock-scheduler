e28e669b137330381ae1ee63aae0288fb37bebb3
Simon Chen <tengentoppa@hotmail.com.tw>
2022-08-08 22:01:02 +0800

    fix(config): fix config

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@ const settings = {
     finMind:
       'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMi0wNi0yMyAyMDoxNzowMyIsInVzZXJfaWQiOiJsb2NodWhzaW4iLCJpcCI6IjExNC4xMzYuNTguMTE1In0.tWMyITWi1PXdSGZGFuE_RcrUKtLYcsrP3DHgnlhZ6oc',
     twelveData: 'b0a59586047a49ac8b9cff87709499d7',
+  },
     server: {
       port: 3000,
     },
-  },
   postgres: {
     user: 'root',
     password: 'root',
