0e0e6a5e23f59eac537119f0e11505d93c504e0b
Simon Chen <tengentoppa@hotmail.com.tw>
2022-08-08 21:56:23 +0800

    fix(config): update config

--- src/config.ts
+++ src/config.ts
@@ -1,33 +1,24 @@
-interface config {
-  [key: string]: any;
-}
-
-const settings: config = {};
-
-settings.twelveData = { plan: 'Grow', global: ['Level A'] };
-
-settings.api = { apicount: 44 * 60 * 24, historyInterval: 4500 };
-
-settings.token = {
-  finMind:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMi0wNi0yMyAyMDoxNzowMyIsInVzZXJfaWQiOiJsb2NodWhzaW4iLCJpcCI6IjExNC4xMzYuNTguMTE1In0.tWMyITWi1PXdSGZGFuE_RcrUKtLYcsrP3DHgnlhZ6oc',
-  twelveData: 'b0a59586047a49ac8b9cff87709499d7',
-};
-
-settings.server = {
-  port: 3000,
-};
-
-settings.postgres = {
-  user: 'root',
-  password: 'root',
-  database: 'postgres',
-  host: 'postgres',
-  port: 5432,
-};
-
-settings.mongodb = {
-  mongoConn: 'mongodb://root:root@mongo:27017/TimeSeries?authSource=admin',
+const settings = {
+  twelveData: { plan: 'Grow', global: ['Level A'] },
+  api: { apicount: 44 * 60 * 24, historyInterval: 4500 },
+  token: {
+    finMind:
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMi0wNi0yMyAyMDoxNzowMyIsInVzZXJfaWQiOiJsb2NodWhzaW4iLCJpcCI6IjExNC4xMzYuNTguMTE1In0.tWMyITWi1PXdSGZGFuE_RcrUKtLYcsrP3DHgnlhZ6oc',
+    twelveData: 'b0a59586047a49ac8b9cff87709499d7',
+    server: {
+      port: 3000,
+    },
+  },
+  postgres: {
+    user: 'root',
+    password: 'root',
+    database: 'postgres',
+    host: 'postgres',
+    port: 5432,
+  },
+  mongodb: {
+    mongoConn: 'mongodb://root:root@mongo:27017/TimeSeries?authSource=admin',
+  },
 };
 
 export default settings;
